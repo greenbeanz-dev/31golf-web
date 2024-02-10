8deb6d7f6e700279366b279326ea1ba30b7d5294
mingg123 <spgb123spgb@gmail.com>
2024-02-10 22:32:05 +0900

    feat: 국내골프 페이지 추가

--- pages/domestic/index.tsx
+++ pages/domestic/index.tsx
@@ -1,12 +1,14 @@
+import { 국내골프Page } from "@component/page/국내골프Page";
 import Head from "next/head";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center w-full h-full">
+    <div className="flex flex-col w-full h-full">
       <Head>
         <title>Home</title>
       </Head>
-      <div>hihi2</div>
+      <div style={{ minHeight: 40 }} />
+      <국내골프Page />
     </div>
   );
 }
--- src/components/organism/ToolBar.tsx
+++ src/components/organism/ToolBar.tsx
@@ -1,11 +1,4 @@
-import {
-  Divider,
-  Link,
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-} from "@nextui-org/react";
+import { Link, Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
 import { useRouter } from "next/router";
 import useLogin from "../../utils/login/useLogin";
 
@@ -31,6 +24,7 @@ const navItem = [
 export default function Topbar() {
   const { pathname } = useRouter();
   const { login, isLogin, logOut, userProfile } = useLogin();
+  const router = useRouter();
 
   return (
     <>
@@ -38,6 +32,8 @@ export default function Topbar() {
         style={{
           backgroundColor: "white",
           zIndex: 1000, // 가장 상위로올림 (다른 탭과 겹칠 경우를 피하기 위함 )
+          borderBottom: "1px solid #e0e0e0",
+          padding: "10px 0 20px",
         }}
         // isBordered
         classNames={{
@@ -53,38 +49,54 @@ export default function Topbar() {
             "data-[active=true]:after:right-0",
             "data-[active=true]:after:h-[2px]",
             "data-[active=true]:after:rounded-[2px]",
-            "data-[active=true]:after:bg-primary",
+            "data-[active=true]:after:bg-sky-900",
           ],
         }}
       >
-        <NavbarBrand>
-          <img src={"/images/logo/31Logo.png"} height={65} width={160} />
-          {/* <Image
-            src="/images/logo/31Logo.png"
+        {/* <NavbarBrand>
+          <img
+            style={{
+              cursor: "pointer",
+            }}
+            src={"/images/logo/31Logo.png"}
+            height={65}
             width={160}
-            height={70}
-            alt="logo"
-          ></Image> */}
-        </NavbarBrand>
-        <NavbarContent className="hidden gap-4 sm:flex" justify="center">
-          {navItem.map((item) => {
-            return (
-              <NavbarItem
-                className="px-4"
-                key={item.label}
-                isActive={pathname === item.href}
-              >
-                <Link color="foreground" href={item.href}>
-                  {item.label}
-                </Link>
-              </NavbarItem>
-            );
-          })}
+            onClick={() => {
+              router.push("/");
+            }}
+          />
+        </NavbarBrand> */}
+        <NavbarContent justify="center" className="gap-20">
+          <>
+            <img
+              style={{
+                cursor: "pointer",
+              }}
+              src={"/images/logo/31Logo.png"}
+              height={65}
+              width={160}
+              onClick={() => {
+                router.push("/");
+              }}
+            />
+            {navItem.map((item) => {
+              return (
+                <NavbarItem
+                  className="px-4"
+                  key={item.label}
+                  isActive={pathname === item.href}
+                >
+                  <Link color="foreground" href={item.href}>
+                    {item.label}
+                  </Link>
+                </NavbarItem>
+              );
+            })}
+          </>
         </NavbarContent>
-        <NavbarItem>{/* <ThemeSwitcher /> */}</NavbarItem>
       </Navbar>
-      <div style={{ height: 24 }} />
-      <Divider />
+      {/* <div style={{ height: 24 }} />
+      <Divider /> */}
     </>
   );
 }
