96b853c1ce70d1662695199e92e37938bc4ff13c
kchoi <kchoi@roundin.kr>
2024-04-10 23:21:55 +0900

    feat: 상품상세 페이지 최종 커밋

---
+++ src/components/organism/상품유의사항.tsx
@@ -0,0 +1,17 @@
+interface 상품유의사항Props {
+  notice: string;
+}
+
+const 상품유의사항 = ({ notice }: 상품유의사항Props) => {
+  return (
+    <div className="flex flex-col max-w-[822px]">
+      <div className="text-xl font-bold">유의사항 및 참조사항</div>
+      <div className="pt-4" />
+      <div className="text-[14px] font-normal text-opacity-70 whitespace-pre text-pretty">
+        {notice}
+      </div>
+    </div>
+  );
+};
+
+export default 상품유의사항;
---
+++ src/components/organism/상품이용특전.tsx
@@ -0,0 +1,17 @@
+interface 상품이용특전Props {
+  benefit: string;
+}
+
+const 상품이용특전 = ({ benefit }: 상품이용특전Props) => {
+  return (
+    <div className="flex flex-col max-w-[822px]">
+      <div className="text-xl font-bold">이용특전 및 참조사항</div>
+      <div className="pt-4" />
+      <div className="text-[14px] font-normal text-opacity-70 whitespace-pre text-pretty">
+        {benefit}
+      </div>
+    </div>
+  );
+};
+
+export default 상품이용특전;
