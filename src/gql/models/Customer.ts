9ae0891f52a6c2425c3b65a4efd483c8b1a11e94
mingg123 <spgb123spgb@gmail.com>
2024-04-20 13:02:40 +0900

    userId, password 제거

--- src/gql/models/Customer.ts
+++ src/gql/models/Customer.ts
@@ -113,8 +113,6 @@ builder.mutationField("createCustomerByWeb", (t) =>
       fax: t.arg.string(),
       isVillain: t.arg.boolean(),
       provider: t.arg.string(),
-      userId: t.arg.string(),
-      password: t.arg.string(),
     },
     resolve: async (query, _parent, _args, _ctx): Promise<any> => {
       const result = await prisma.customer.create({
@@ -129,8 +127,6 @@ builder.mutationField("createCustomerByWeb", (t) =>
           created_at: new Date(Date.now()).toISOString(),
           updated_at: new Date(Date.now()).toISOString(),
           provider: _args.provider,
-          userid: _args.userId,
-          password: _args.password,
         },
       });
       return result;
@@ -149,8 +145,6 @@ builder.mutationField("UpdateCustomerByIdWeb", (t) =>
       memo: t.arg.string(),
       fax: t.arg.string(),
       isVillain: t.arg.boolean(),
-      userId: t.arg.string(),
-      password: t.arg.string(),
     },
     resolve: async (query, _parent, _args, _ctx): Promise<any> => {
       const result = await prisma.customer.update({
@@ -166,8 +160,6 @@ builder.mutationField("UpdateCustomerByIdWeb", (t) =>
           fax: _args.fax,
           is_villain: _args.isVillain,
           updated_at: new Date(Date.now()).toISOString(),
-          userid: _args.userId,
-          password: _args.password,
         },
       });
       return result;
