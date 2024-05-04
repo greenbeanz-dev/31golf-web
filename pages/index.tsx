ee8cb9bd51b4101317931b6a82ef61b0704ebdc3
handy <kyeungseok.kang@gmail.com>
2024-05-04 17:24:47 +0900

    refact : 디테일 페이지 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,3 @@ function Home() {
 }
 
 export default Home;
-
-export async function getServerSideProps() {
-  // Fetch data from external API
-  // const res = await fetch(`https://.../data`);
-  // const data = await res.json();
-
-  // Pass data to the page via props
-  return { props: {} };
-}
