977c90cb1d2deab262bbacbf09e79305c159692e
handy <kyeungseok.kang@gmail.com>
2024-08-16 14:00:40 +0900

    feat : 로딩 개선

--- src/components/Product/베스트상품Component.tsx
+++ src/components/Product/베스트상품Component.tsx
@@ -1,14 +1,15 @@
 import gqlClient from "@/gql/gqlClient";
 import { ProductListInfinityQuery } from "@/gql/query/product/crud";
+import { Skeleton, cn } from "@nextui-org/react";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useIsMobile } from "../../hooks/useIsMobile";
-
 const defaultImage = "/images/logo/golf_img1.png";
 
 export function 베스트상품Component() {
   const data = useQuery({
+    queryKey: ["productListInfinity"],
     queryFn: async () => {
       const response = await gqlClient.request(ProductListInfinityQuery, {
         isBest: true,
@@ -75,3 +76,59 @@ export function 베스트상품Component() {
     </div>
   );
 }
+
+export const 베스트상품ComponentSkleleton = () => {
+  const isMobile = useIsMobile();
+  return (
+    <>
+      <div
+        className={`flex w-full max-w-[95vw] justify-between overflow-x-auto gap-4 overflow-y-hidden`}
+      >
+        {Array.from({
+          length: 3,
+        }).map((_, index) => {
+          return (
+            <div
+              key={index}
+              className="cursor-pointer h-[320px]"
+              style={{
+                width: isMobile ? 256 : 292,
+                minWidth: isMobile ? 256 : 292,
+                cursor: "pointer",
+              }}
+            >
+              <Skeleton
+                className={cn(
+                  isMobile ? "w-[256px] h-[343px]" : "w-[292px] h-[223px]",
+                  "rounded-2xl"
+                )}
+              >
+                <div
+                  className={
+                    isMobile ? "w-[256px] h-[343px]" : "w-[292px] h-[223px]"
+                  }
+                ></div>
+              </Skeleton>
+              <div className="min-h-4" />
+              <Skeleton className="rounded">
+                <div className="text-[16px] font-bold leading-6">title</div>
+              </Skeleton>
+              <div className="pt-1" />
+              <Skeleton className="rounded">
+                <div className="text-[16px] font-normal w-full overflow-ellipsis truncate overflow-hidden leading-normal tracking-tight opacity-70">
+                  Summary
+                </div>
+              </Skeleton>
+              <div className="pt-1" />
+              <Skeleton className="rounded">
+                <div className="text-[20px] font-bold text-[#0A7BE4] leading-6 tracking-tight">
+                  Price
+                </div>
+              </Skeleton>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
